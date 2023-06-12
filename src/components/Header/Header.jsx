2cce21bdb4a37371a486dcd76ca9f5f784e35c07
Daniela <danielarios27.dr@gmail.com>
2023-06-12 17:33:04 -0500

    feat: Agrego el navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,39 @@
+import { Link } from "react-router-dom";
+import "./Header.css";
+import { Menu } from "../Icons";
+import { useState } from "react";
+
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  console.log(isOpen);
   return (
-    <header>Header</header>
-  )
-}
+    <header>
+      <div className="logo">
+        <span className="logo__title">
+          <Link to="/">DR</Link>
+        </span>
+        <span className="logo__sub-title">Panel de control</span>
+      </div>
+      <nav className="nav">
+        <ul
+          className={`nav__list ${
+            isOpen ? "nav__list--open" : "nav__list--close"
+          }`}
+        >
+          <li className="nav__item" onClick={toggleMenu}>
+            <Link to="productos">Productos</Link>
+          </li>
+          <li className="nav__item" onClick={toggleMenu}>
+            <Link to="pedidos">Pedidos</Link>
+          </li>
+        </ul>
+        <div className="nav__menu" onClick={toggleMenu}>
+          <Menu />
+        </div>
+      </nav>
+    </header>
+  );
+};
---
+++ src/components/Icons.jsx
@@ -0,0 +1,29 @@
+export function Menu() {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4 18L20 18"
+        stroke="#646cff"
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
+      <path
+        d="M4 12L20 12"
+        stroke="#646cff"
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
+      <path
+        d="M4 6L20 6"
+        stroke="#646cff"
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
