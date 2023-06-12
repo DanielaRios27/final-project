a6605c5f2dbc3b464da804dbbbcfa52fb7973674
Daniela <danielarios27.dr@gmail.com>
2023-06-12 18:47:25 -0500

    feat: Productos iniciales

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,16 @@
+import products from "../../mocks/products.json";
+import { useFilters } from "../../hooks/useFilters";
+import { Products } from "../../components/Products/Products";
+import "./Home.css";
+
 export const Home = () => {
+  const { filterProducts } = useFilters();
+  console.log(filterProducts);
+  const filteredProducts = filterProducts(products);
+  console.log(filteredProducts);
   return (
-    <main>Home</main>
-  )
-}
+    <main>
+      <Products products={filteredProducts} />
+    </main>
+  );
+};
